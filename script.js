363d1d29227ee2906815e67dcb5e7585a8de5747
Mohammad Ishan Ahmad <mdishanahmad@gmail.com>
2026-04-13 18:29:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
+let tableEl = document.querySelector("#myTable");
 function createTable() {
-    //Write your code here
-  
+  tableEl.innerHTML="";
+  let rn = Number(prompt("Input number of rows"));
+  let cn = Number(prompt("Input number of columns"));
+
+  if (isNaN(rn) || isNaN(cn)) {
+    return;
+  }
+  if (rn <= 0 || cn <= 0) {
+    alert("Enter valid positive numbers");
+    return;
+  }
+
+  let tableBodyEl = document.createElement("tbody");
+
+  for (let i = 0; i < rn; i++) {
+    let tableRow = document.createElement("tr");
+
+    for (let j = 0; j < cn; j++) {
+      let tableData = document.createElement("td");
+      tableData.textContent = `Row-${i} Column-${j}`;
+      tableRow.append(tableData);
+    }
+    tableBodyEl.append(tableRow);
+  }
+
+  tableEl.append(tableBodyEl);
 }
